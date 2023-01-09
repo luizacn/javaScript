26d7f2514e1b1d8809b594cb91f2c6ba4bdfbda9
luizacn <luizcna2022@gmail.com>
2023-01-09 15:08:07 -0300

    atualizações ex008, 009 , 010

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 30
+
+console.log(`A velocidade do carro é ${vel} km por hora.`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
