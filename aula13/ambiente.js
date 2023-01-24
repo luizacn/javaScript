645d66d920dd659150f0dfeecba83b95281e958f
luizacn <luizcna2022@gmail.com>
2023-01-23 22:00:05 -0300

    criei aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+/*console.log('Tudo bem?')*/
+
+/*var contador = 1
+while( contador <= 6 ){
+    console.log(`Número ${contador}`)
+    contador++
+}*/
+
+var contador = 1
+
+do {
+    console.log(`Número ${contador}`)
+    contador++
+} while (contador <= 6)
